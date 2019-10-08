e9d7adb496a10c787577455b4cc7252f62d01cc9
F17 <szerlag.kamil@gmail.com>
2019-10-08 18:34:12 +0200

    Update Main.java

--- simpleApps/Composition/Main.java
+++ simpleApps/Composition/Main.java
@@ -3,12 +3,12 @@ package com.Kamil;
 public class Main {
 
     public static void main(String[] args) {
-        Furniture theFurniture = new Furniture("black", "Big", "forTwo");
+        Furniture theFurniture = new Furniture("black", "small", "fromRussia");
         Room theRoom = new Room("dark", "glassy", "dywan", theFurniture);
 
 
         theRoom.getFurniture().hoveringTheCarpet("wziuuu!");
-        theRoom.wpuscAbyPoruchac();
+        theRoom.cleanTheWindow();
 
     }
 }
